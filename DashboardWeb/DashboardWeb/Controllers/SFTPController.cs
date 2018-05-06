fd59a494e1ad6a80b24b412e378073224e145696
Hasan KHAN <Hasan_m_khan@outlook.com>
2018-05-06 00:01:18 -0400

    Start Point

---
+++ DashboardWeb/DashboardWeb/Controllers/SFTPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DashboardWeb.Controllers
+{
+    public class SFTPController : Controller
+    {
+        // GET: SFTP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
